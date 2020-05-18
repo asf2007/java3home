0ea09078000332190a3b0ba3c0e97294ce8547dc
asf2007 <leha.flyagin@mail.ru>
2020-05-18 14:07:46 +0300

    home1

---
+++ src/task3/Fruit.java
@@ -0,0 +1,8 @@
+package task3;
+
+public interface Fruit {
+
+    String getName();
+
+    double getWeight();
+}
---
+++ src/task3/Orange.java
@@ -0,0 +1,13 @@
+package task3;
+
+public class Orange implements Fruit {
+    @Override
+    public String getName() {
+        return "Orange";
+    }
+
+    @Override
+    public double getWeight() {
+        return 1.5;
+    }
+}
